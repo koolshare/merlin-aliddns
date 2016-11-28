005e1944dbb4565877346ca7175fce0df5b94729
lixin <sadoneli@gmail.com>
2016-11-28 10:41:28 +0800

    first commit

---
+++ config.json.js
@@ -0,0 +1,9 @@
+{
+    "build_date": "2016-09-27_18:43:33", 
+    "description": "阿里云解析自动更新IP", 
+    "home_url": "Module_aliddns.asp", 
+    "md5": "7a70f0ffe2852c378fc2cc289158f496", 
+    "module": "aliddns", 
+    "title": "aliddns", 
+    "version": "0.4"
+}
