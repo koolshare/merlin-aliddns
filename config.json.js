53d69ed1c2ea1294fc42e58545227172802be4e6
lixin <sadoneli@gmail.com>
2016-12-08 16:12:55 +0800

    update version to 0.7

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2016-09-27_18:43:33", 
     "description": "阿里云解析自动更新IP", 
     "home_url": "Module_aliddns.asp", 
-    "md5": "83538fa233ba8332c56fab0c8fd85de1", 
+    "md5": "85ce77a22ba1fd0d3c1203e5999b1352", 
     "module": "aliddns", 
     "title": "aliddns", 
-    "version": "0.6"
+    "version": "0.7"
 }
