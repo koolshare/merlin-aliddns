16418508c04d97cd7f9126f6a867b41df1a9900c
lixin <sadoneli@gmail.com>
2017-04-01 10:24:47 +0800

    update aliddns

--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2016-09-27_18:43:33", 
     "description": "阿里云解析自动更新IP", 
     "home_url": "Module_aliddns.asp", 
-    "md5": "85ce77a22ba1fd0d3c1203e5999b1352", 
+    "md5": "015d284fc2d5e04b0497e20980464508", 
     "module": "aliddns", 
     "title": "aliddns", 
-    "version": "0.7"
+    "version": "0.8"
 }
