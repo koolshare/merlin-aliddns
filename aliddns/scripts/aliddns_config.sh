2ca2532af319fed3a9e5900b3224f6c7f035f12c
sadoneli <sadoneli@gmail.com>
2018-10-02 21:30:34 +0800

    update aliddns

--- aliddns/scripts/aliddns_config.sh
+++ aliddns/scripts/aliddns_config.sh
@@ -1,11 +1,44 @@
 #!/bin/sh
 
-if [ "`dbus get aliddns_enable`" = "1" ]; then
-    dbus delay aliddns_timer `dbus get aliddns_interval` /koolshare/scripts/aliddns_update.sh
-    # run once after submit
-	sleep 2
+source /koolshare/scripts/base.sh
+eval `dbus export aliddns`
+
+start_aliddns(){
+	cru a aliddns_checker "*/$aliddns_interval * * * * /koolshare/scripts/aliddns_update.sh"
 	sh /koolshare/scripts/aliddns_update.sh
-else
-    dbus remove __delay__aliddns_timer
-    nvram set ddns_hostname_x=`nvram get ddns_hostname_old`
-fi
+
+	if [ ! -L "/koolshare/init.d/S98Aliddns.sh" ]; then 
+		ln -sf /koolshare/scripts/aliddns_config.sh /koolshare/init.d/S98Aliddns.sh
+	fi
+}
+
+stop_aliddns(){
+	jobexist=`cru l|grep aliddns_checker`
+	# kill crontab job
+	if [ -n "$jobexist" ];then
+		sed -i '/aliddns_checker/d' /var/spool/cron/crontabs/* >/dev/null 2>&1
+	fi
+	nvram set ddns_hostname_x=`nvram get ddns_hostname_old`
+}
+
+case $ACTION in
+start)
+	if [ "$aliddns_enable" == "1" ];then
+		logger "[软件中心]: 启动阿里DDNS！"
+		start_aliddns
+	else
+		logger "[软件中心]: 阿里DDNS未设置开机启动，跳过！"
+	fi
+	;;
+stop)
+	stop_aliddns
+	;;
+*)
+	if [ "$aliddns_enable" == "1" ];then
+		start_aliddns
+	else
+		stop_aliddns
+	fi
+	http_response "$1"
+	;;
+esac
--- config.json.js
+++ config.json.js
@@ -2,8 +2,8 @@
     "build_date": "2016-09-27_18:43:33", 
     "description": "阿里云解析自动更新IP", 
     "home_url": "Module_aliddns.asp", 
-    "md5": "95e67b8864a827e2c340df8ddea55be7", 
+    "md5": "2a76a51fb5ad70118d0655d9d846f7f3", 
     "module": "aliddns", 
     "title": "aliddns", 
-    "version": "1.0"
+    "version": "1.1"
 }
